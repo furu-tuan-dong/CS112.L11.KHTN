e893e3fd17d405ab62ce1ac7d22ef3f99c8e78b2
DatDoc <uncledrew2youngblood@gmail.com>
2020-10-31 21:20:52 +0700

    done

--- Week7/dia_lan_test_case.py
+++ Week7/dia_lan_test_case.py
@@ -1,8 +1,9 @@
 import numpy as np
 import itertools
+import os
 
 
-def check_result(k_items):
+def check_result(k_items): # check if the combination is satisfy or not
     res = k_items[0]
     for item in k_items:
         res = res&item
@@ -12,7 +13,7 @@ def check_result(k_items):
         return False
 
 
-def brute_force(arr, k):
+def brute_force(arr, k): # loop through all the combinations in the array and find the possible outcome
     combinations = itertools.combinations(arr, k)
     for combination in combinations:
         if check_result(combination):
@@ -20,22 +21,33 @@ def brute_force(arr, k):
     return "NO", None
 
 
-def generate_rules(n, k, lower_bound, upper_bound):
+def generate_arr_elements(n, k, lower_bound, upper_bound): # 0 ≤ elements < 2^12 
     arr = np.random.randint(low=lower_bound, high=upper_bound + 1, size=n)
     return arr
 
 
-def generate_multiple_tests(low_n, high_n, num_tests, low_ai=0, high_ai=4095):
-    ns = np.random.randint(low=low_n, high=high_n, size=num_tests)
+def generate_multiple_tests(lower_bound_testcase, upper_bound_testcase, num_tests, filename, \
+                            low_ai=0, high_ai=4095):
+    ns = np.random.randint(low=lower_bound_testcase, high=upper_bound_testcase, size=num_tests)
+    f = open(filename, "a")
     for i in range(len(ns)):
-        k = np.random.randint(low=1, high=ns[i]+1)
-        arr = generate_rules(ns[i], k, low_ai, high_ai)
+        k = np.random.randint(low=1, high=ns[i]+1) 
+        arr = generate_arr_elements(ns[i], k, low_ai, high_ai)
         output, explain = brute_force(arr, k)
+        arr = ' '.join(map(str, arr)) # convert array to string
         print(f'Input:\n{ns[i]} {k}\n{arr}\nOutput:\n{output}')
+        f.write(f'Input:\n{ns[i]} {k}\n{arr}\nOutput:\n{output}\n')
         if explain:
             print('Explain:\nk elements are:', explain)
-        print()
-
+            f.write('Explain:\nk elements are: ' + str(explain).replace("(", "").replace(")", "") + "\n")
+    f.close()
 
 if __name__ == "__main__":
-    generate_multiple_tests(low_n=1, high_n=20, num_tests=3)
+    distributions = [0, 100, 10000, 20000] # 1 ≤ n ≤ 2×10^4
+    filename = "result.txt"
+    f = open(filename, "a")
+    for i in range(len(distributions) - 1):
+        print("10 tests from {} to {}".format(distributions[i] + 1, distributions[i+1]))
+        f.write("10 tests from {} to {}".format(distributions[i] + 1, distributions[i+1]))
+        generate_multiple_tests(distributions[i] + 1, distributions[i+1], 10, filename)
+    f.close()
